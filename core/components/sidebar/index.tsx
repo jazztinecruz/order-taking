60bbe2611ec5072979cdce7841bb266c749ff17a
jazztinecruz <me@jazztinecruz.com>
2024-11-06 21:36:11 +0800

    feat(sidebar): highlight active link in sidebar based on current pathname

--- core/components/sidebar/index.tsx
+++ core/components/sidebar/index.tsx
@@ -2,19 +2,26 @@
 
 import Link from "next/link";
 import { SIDEBARLINKS } from "./constants";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <aside className="w-52 p-4 border-r-2">
       <ul className="flex items-center flex-col gap-4">
-        {SIDEBARLINKS.map((link) => (
-          <Link
-            key={link.url}
-            href={link.url}
-            className="w-full p-2 rounded-lg text-left hover:bg-slate-200 cursor-pointer">
-            {link.title}
-          </Link>
-        ))}
+        {SIDEBARLINKS.map((link) => {
+          const isActive = pathname === link.url;
+          return (
+            <Link
+              key={link.url}
+              href={link.url}
+              className={`w-full p-2 rounded-lg text-left hover:bg-slate-200 cursor-pointer ${
+                isActive && "font-bold bg-slate-200"
+              }`}>
+              {link.title}
+            </Link>
+          );
+        })}
       </ul>
     </aside>
   );
