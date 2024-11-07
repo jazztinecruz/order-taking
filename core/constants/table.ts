013b91c52f0d179b5df7bed7ade8133feb6ad844
jazztinecruz <me@jazztinecruz.com>
2024-11-07 11:14:16 +0800

    feat(constants): add table header constants for products, customers, and orders

---
+++ core/constants/table.ts
@@ -0,0 +1,24 @@
+export const PRODUCTTABLEHEADERS = [
+  "Name",
+  "Code",
+  "Unit Price",
+  "isActive",
+  "Image",
+  "Actions",
+];
+
+export const CUSTOMERTABLEHEADERS = [
+  "Full Name",
+  "Mobile Number",
+  "City",
+  "isActive",
+  "Actions",
+];
+
+export const ORDERSTABLEHEADERS = [
+  "Customer",
+  "Delivery Date",
+  "Status",
+  "Amount Due",
+  "Actions",
+];
