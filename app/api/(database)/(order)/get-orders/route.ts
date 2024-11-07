057ba3d39640b5df1b0581926e997e2f46469165
jazztinecruz <me@jazztinecruz.com>
2024-11-07 11:14:32 +0800

    feat(order): include customer details in getOrders response

--- app/api/(database)/(order)/get-orders/route.ts
+++ app/api/(database)/(order)/get-orders/route.ts
@@ -3,7 +3,11 @@ import { prisma } from "@/prisma";
 
 export const GET = async () => {
   try {
-    const orders = await prisma.order.findMany();
+    const orders = await prisma.order.findMany({
+      include: {
+        customer: true,
+      },
+    });
     return NextResponse.json(orders, { status: 200 });
   } catch (error) {
     console.error(error);
