2289747afecf90872d595501c5820a111efc592b
jazztinecruz <me@jazztinecruz.com>
2024-11-07 19:55:43 +0800

    feat(button): add disabled state styling to button component

--- core/components/button/index.tsx
+++ core/components/button/index.tsx
@@ -6,7 +6,9 @@ type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 
 const Button = ({ children, ...props }: Props) => {
   return (
-    <button {...props} className="bg-blue-500 text-white rounded-md p-3">
+    <button
+      {...props}
+      className="bg-blue-500 text-white rounded-md p-3 disabled:bg-gray-400 disabled:cursor-not-allowed">
       {children}
     </button>
   );
